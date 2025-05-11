8563b1cda1598d47c3ba3af5155c9579f7a80b6f
AP1905 <anuja13122004@gmail.com>
2025-05-11 13:48:40 +0530

    Modified Home Section

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let menuList = document.getElementById("menu");
 let menuBtn = document.querySelector(".menuBtn");
 let closeBtn = document.querySelector(".closeBtn");
 
+let footerP = document.getElementById("footer-p");
+
 function openMenu() {
   menuList.style.display = "block";
   menuBtn.style.display = "none";
@@ -13,3 +15,6 @@ function closeMenu() {
   closeBtn.style.display = "none";
   menuBtn.style.display = "block";
 }
+
+let d = new Date();
+footerP.innerText = `\u00A9 ${d.getFullYear()} Anuja Pawar | Frontend Developer`
